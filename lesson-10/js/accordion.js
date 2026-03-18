586c0d6581200c40faae95e230a0e99e718b792d
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-03-18 11:05:25 -0400

    a

--- lesson-10/js/accordion.js
+++ lesson-10/js/accordion.js
@@ -1,11 +1,10 @@
 // STEP 6: Nab all the SUMMARY elements
 
-
 // STEP 7: Loop through the element array to add an event listener, and reference a function called closeOpenDetails
-
 
 // STEP 8a: Create the closeOpenDetails() function
 
+		
 	// STEP 8b: Loop through the summaries array again
 
 		// STEp 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
